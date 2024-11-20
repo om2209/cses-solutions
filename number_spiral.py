5fd68c623a618e775ab1133aff7e82784cae1b74
Omkar Mishra <omkarmishra66@gmail.com>
2024-11-21 00:23:26 +0530

    Added python solution for number spiral problem

---
+++ number_spiral.py
@@ -0,0 +1,27 @@
+def number_spiral(y: int, x: int) -> int:
+    if y == 1:
+        if x % 2 == 1:
+            return x**2
+        else:
+            return (x-1)**2 + 1
+    elif x == 1:
+        if y % 2 == 0:
+            return y**2
+        else:
+            return (y-1)**2 + 1
+    elif x <= y:
+        if y % 2 == 0:
+            return y**2 - (x - 1)
+        else:
+            return (y-1)**2 + x
+    else:
+        if x % 2 == 1:
+            return x**2 - (y - 1)
+        else:
+            return (x-1)**2 + y
+        
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        y, x = list(map(int, input().strip().split()))
+        print(number_spiral(y, x))
